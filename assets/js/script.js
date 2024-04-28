4400920ed0c3ed4624fead0ab0194ca16cedfbd3
aliceC119 Chan <alicebay10@yahoo.co.uk>
2024-04-28 16:46:36 +0000

    Add some styles and  js to make choice buttons responsive, and add a footer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,54 @@
-console.log("Connected!")
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random()* 5)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "It's a DRAW!"
+        
+    }
+    else{
+        switch(playerChoice){
+            case "scissors":
+                result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
+                break; 
+            case "rock":
+                result = (computerChoice === "lizard") ? "You Win!" : "You Lose!";
+                break;     
+            case "lizard":
+                result = (computerChoice === "spock") ? "You Win!" : "You Lose!";
+                break; 
+            case "spock":
+                result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
+                break;  
+            case "scissors":
+                result = (computerChoice === "lizard") ? "You Win!" : "You Lose!";
+                break; 
+            case "lizard":
+                result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+                break; 
+            case "paper":
+                result = (computerChoice === "spock") ? "You Win!" : "You Lose!";
+                break;   
+            case "spock":
+                result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
+                break;
+            case "rock":
+                result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
+                break;           
+        }
+    }
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    
+}
