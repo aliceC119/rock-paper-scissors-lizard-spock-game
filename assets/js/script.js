3c3037155f82c0fdc335da09c974372506b4ab3a
aliceC119 Chan <alicebay10@yahoo.co.uk>
2024-04-29 16:31:30 +0000

    Add some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,52 @@
+
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
+let player = 0;
+let computer = 0;
+let numOfRound = 0;
+
+
+function playTurn(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random()* 5)];
+    let resultInt = 0;
+
+    if(playerChoice === computerChoice){
+        resultInt = 0;
+        
+    }
+    else{
+        switch(playerChoice){
+            case "scissors":
+                resultInt = (computerChoice === "paper" || computerChoice ==="lizard") ? 1 : -1;
+                break;
+            case "paper":
+                resultInt = (computerChoice === "rock" || computerChoice ==="spock") ? 1 : -1;
+                break; 
+            case "rock":
+                resultInt = (computerChoice === "lizard"|| computerChoice ==="scissors") ? 1 : -1;
+                break;     
+            case "lizard":
+                resultInt = (computerChoice === "spock" || computerChoice ==="paper") ? 1 : -1;
+                break; 
+            case "spock":
+                resultInt = (computerChoice === "scissors"|| computerChoice ==="rock") ? 1 : -1;
+                break;  
+                  
+        }
+    }
+
+    return resultInt;
+}
+
 function playGame(playerChoice){
 
-    const computerChoice = choices[Math.floor(Math.random()* 5)];
+   
+    
     let result = "";
+
 
     if(playerChoice === computerChoice){
         result = "It's a DRAW!"
@@ -15,7 +55,7 @@ function playGame(playerChoice){
     else{
         switch(playerChoice){
             case "scissors":
-                result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+                result = (computerChoice === "paper") ? "You Win" : "You Lose!";
                 break;
             case "paper":
                 result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
@@ -45,10 +85,33 @@ function playGame(playerChoice){
                 result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
                 break;           
         }
-    }
+    
+
+
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
     resultDisplay.textContent = result;
 
+    playerOptions.forEach(option => {
+        option.addEventListener('click',function(){
+
+            const movesLeft = document.querySelector('.movesleft');
+            moves++;
+            movesLeft.innerText = `Moves Left: ${10-moves}`;
+            
+
+            const choiceNumber = Math.floor(Math.random()*3);
+            const computerChoice = computerOptions[choiceNumber];
+
+            // Function to check who wins
+            winner(this.innerText,computerChoice)
+            
+            // Calling gameOver function after 10 moves
+            if(moves == 10){
+                gameOver(playerOptions,movesLeft);
+            }
+        })
+    })
     
+}
 }
