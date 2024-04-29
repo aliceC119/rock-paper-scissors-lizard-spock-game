6169cc4e1c8159baa99b6560cd3acffa92b50a64
aliceC119 Chan <alicebay10@yahoo.co.uk>
2024-04-29 23:19:39 +0000

    change styles to background color and button colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,21 @@ const numOfTurn = 10;
 let playerScore = 0;
 let computerScore = 0;
 let moves = 10;
-let meetEndGameCondition = false;
+let meetEndGameCondition = false; 
 
-
-/**
- * Set up the game logic */
+/* Set up the game logic */
 
 function playTurn(playerChoice, computerChoice) {
-    
-    
     let resultInt = 0;
+
+/* When playerChoice and computerChoice are the same, then both sides scored 0 */
 
     if(playerChoice === computerChoice){
         resultInt = 0;
         
     }
+/*  Otherwise the chosen choice beats the relative two choices */
+
     else{
         switch(playerChoice){
             case "scissors":
@@ -50,18 +50,22 @@ function playTurn(playerChoice, computerChoice) {
     return resultInt;
 }
 
-function playGame(playerChoice){
+/*  Set the computer choose randomly within the 5 choices */
 
+function playGame(playerChoice){
     let computerChoice = choices[Math.floor(Math.random()* 5)];
+
+/*  Link the relative images to computerChoice and playerChoice when play button is clicked */
+
     document.getElementById("computer-choice").src = imagePath + computerChoice + ".png";
     document.getElementById("player-choice").src = imagePath + playerChoice + ".png";
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;  
+
+/* Different results will be showed if the result conditions are met */
 
     let resultInt = playTurn(playerChoice, computerChoice);
-  
     let result = "";
-
     if (resultInt > 0) {
         result = "You Win!";
         playerScore++;
@@ -69,7 +73,7 @@ function playGame(playerChoice){
         result = "You Lose!";
         computerScore++;
     } else {
-        result = "It's a Draw!"
+        result = "It's a DRAW!";
     }
     playerScoreDisplay.textContent = "Score: " + playerScore.toString();
     computerScoreDisplay.textContent = "Score: " + computerScore.toString();
@@ -77,38 +81,40 @@ function playGame(playerChoice){
 
     moves++;
     movesLeft.innerText = `Moves Left: ${numOfTurn - moves}`;
-
+    
     if(moves >= numOfTurn || meetEndGameCondition){
+        
         endGame();
     }
 
-    function endGame() {
-        let winner = "It's a Tie Game";
-        if (playerScore > computerScore) {
-            winner = "The winner is" + "You"; 
-        } else if (computerScore > playerScore) {
-            winner = "The winner is " + "Computer";
-        }
-        resultDisplay.textContent = winner;
-
-        document.querySelector('.choice').style.display = 'none';
-        document.querySelector('.move').style.display = 'none';
-    }
-    function clearDisplay() {
-        playerScore = 0;
-        computerScore = 0;
-        moves = 0;
-
-        movesLeft.innerText = `Moves Left: ${10-moves}`;
-        playerScoreDisplay.textContent = "Score: " + playerScore.toString();
-        computerScoreDisplay.textContent = "Score: " + computerScore.toString();
-        resultDisplay.textContent = "";
-
-        document.querySelector('.choices').style.display = 'block';
-        document.querySelector('.move').style.display = 'block;'
-    }
-
+    
 }
 
+function endGame() {
+    //just an example
+    let winner = "It's a Tie Game";
+    if (playerScore > computerScore) {
+        winner =  "The winner is " + "You";
+    } else if (computerScore > playerScore) {
+        winner =  "The winner is " + "Computer";
+    }
+    resultDisplay.textContent = winner;
 
- 
+    document.querySelector('.choices').style.display = 'none';
+    document.querySelector('.move').style.display = 'none';
+}
+/* Each game has 10 moves, 1 move is subtracted after each move.*/
+
+function clearDisplay(){
+    playerScore = 0;
+    computerScore = 0;
+    moves = 0;
+
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+    playerScoreDisplay.textContent = "Score: " + playerScore.toString();
+    computerScoreDisplay.textContent = "Score: " + computerScore.toString();
+    resultDisplay.textContent = "";    
+
+    document.querySelector('.choices').style.display = 'block';
+    document.querySelector('.move').style.display = 'block';
+}
